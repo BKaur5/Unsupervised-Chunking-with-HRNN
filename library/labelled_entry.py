e6f3efa0a49a94035de3a00329ad1b9446a7f8a2
BKaur5 <baksheesh2001@gmail.com>
2023-10-31 14:18:00 -0600

    added a new function

--- library/labelled_entry.py
+++ library/labelled_entry.py
@@ -1,6 +1,10 @@
 class LabelledEntry:
-    def __init__(self, bracketed_sent):
-        self.chunks = list(map(str.split, bracketed_sent[1:-1].split('] [')))
+    def __init__(self, chunks):
+        self.chunks = chunks
+    
+    def load_from_bracket_format(bracketed_sent):
+        chunks = list(map(str.split, bracketed_sent[1:-1].split('] [')))
+        return LabelledEntry(chunks)
 
     def load_from_IB_format(words, tags):
         # list(zip(map(lambda x: x[0], test.pkl), test_tag.pkl))
@@ -10,7 +14,7 @@ class LabelledEntry:
                 bracketed_sent += '] ['
             bracketed_sent += word+' '
         bracketed_sent = bracketed_sent[2:-1]
-        return LabelledEntry(bracketed_sent)
+        return LabelledEntry.load_from_bracket_format(bracketed_sent)
     
     def load_from_12_format(words, tags):
         # test.pkl
@@ -20,7 +24,7 @@ class LabelledEntry:
             bracketed_sent += word
             if tag=='2':
                 bracketed_sent += '] ['
-        return LabelledEntry(bracketed_sent)
-        
+        return LabelledEntry.load_from_bracket_format(bracketed_sent)
+
     def __str__(self):
         return '['+'] ['.join(list(map(' '.join, self.chunks)))+']'

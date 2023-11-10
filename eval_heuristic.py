2e0d541902b6a919fcf7f64ec44a39ce83ad46cc
BKaur5 <baksheesh2001@gmail.com>
2023-11-09 21:30:08 -0700

    Added these files (same as in Behzad's repo). Only changed the file path

---
+++ eval_heuristic.py
@@ -0,0 +1,32 @@
+import pickle
+from tqdm import tqdm
+from library.HRNN import validation_output
+from library.HRNN import eval_conll2000
+
+def eval_hu(test_data, test_tags_gt, single_words=False, **args):
+    output = ""
+    for i, (pair, true_tag) in tqdm(enumerate(zip(test_data, test_tags_gt)), total=len(test_data)):
+        if single_words:
+            ind = [1 for t in pair[1]]
+        else:
+            ind = [t=='2' for t in pair[1]]
+        vo = validation_output(ind, true_tag)
+        output += vo
+    fscore, acc = eval_conll2000(output, **args)
+    return fscore, acc
+
+
+if __name__ == "__main__":
+    # TEST_PATH = "experiments/second_order_hiddendim_anal_FA/test_predicted_0.pkl"
+    TEST_PATH = "data/en_sentences/test.pkl"
+    TEST_TRUE_TAGS_PATH = "data/en_BI_tags/test.pkl"
+
+    test_data = pickle.load(open(TEST_PATH, 'rb'))
+    test_tags_gt = pickle.load(open(TEST_TRUE_TAGS_PATH, 'rb'))#[:len(test_data)]
+    print(len(test_data), len(test_tags_gt))
+    fscore, acc = eval_hu(test_data, test_tags_gt)
+    print( " __________________________________")
+    print(f"| Test:")
+    print(f"|     F1:       {fscore}")
+    print(f"|     Accuracy: {acc}")
+    print( "|__________________________________")

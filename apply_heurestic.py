45a4c10a022156eb5bb517d7e8ec98343f29a9a6
BKaur5 <baksheesh2001@gmail.com>
2023-11-22 22:12:02 -0700

    Added code to make bracketed sentence output files for chunks according to single and double word heuristics

--- apply_heurestic.py
+++ apply_heurestic.py
@@ -1,6 +1,25 @@
 from library.heurestics import double_word_heurestic, single_word_heurestic
+import pickle
 
-TEST_DATA_PATH = "HRNNdata_en/test.pkl"
-TEST_TAGS_PATH = "HRNNdata_en/test_tag.pkl"
+def heuristic_output_in_bracketed_form(original_sentences_file, output_file, heuristic):
+    data = pickle.load(open(original_sentences_file, 'rb'))
+    with(open(output_file, 'w')) as f:
+        for row in data:
+            sentence = ' '.join(row)
+            if heuristic == 'single':
+                chunks = single_word_heurestic(sentence)
+            elif heuristic == 'double':
+                chunks = double_word_heurestic(sentence)
+            f.write(str(chunks)+"\n")
 
-# apply all heurestics and write files
+if __name__ == "__main__":
+    single_word_hu_ouputs = ["data/en_new_notation_single_word_hu/test.txt", "data/en_new_notation_single_word_hu/val.txt", "data/en_new_notation_single_word_hu/train.txt"]
+    double_word_hu_ouputs = ["data/en_new_notation_double_word_hu/test.txt", "data/en_new_notation_double_word_hu/val.txt", "data/en_new_notation_double_word_hu/train.txt"]
+    original_data_files = ["data/en_sentences/test.pkl", "data/en_sentences/val.pkl", "data/en_sentences/train.pkl"]
+
+    for i in range(3):
+        original_sentences_file = original_data_files[i]
+        single_output = single_word_hu_ouputs[i]
+        double_output = double_word_hu_ouputs[i]
+        heuristic_output_in_bracketed_form(original_sentences_file, single_output, 'single')
+        heuristic_output_in_bracketed_form(original_sentences_file, double_output, 'double')

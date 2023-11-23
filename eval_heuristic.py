ed9c78e52f6111baef15c292a8b69377f6f2bae9
BKaur5 <baksheesh2001@gmail.com>
2023-11-22 21:19:51 -0700

    changed the whole evaluation logic and inputs to files with bracketed sentences

--- eval_heuristic.py
+++ eval_heuristic.py
@@ -1,23 +1,13 @@
-import pickle
-from tqdm import tqdm
-from library.HRNN import validation_output
-from library.eval_conll_python import conlleval
-
-
-def eval_hu(test_data, test_tags_gt, single_words=False, **args):
-    output = ""
-    for i, (pred_tag, true_tag) in tqdm(enumerate(zip(test_data, test_tags_gt)), total=len(test_data)):
-        if single_words:
-            ind = [1 for t in pred_tag]
-        else:
-            ind = [t == '2' for t in pred_tag]
-        vo = validation_output(ind, true_tag)
-        output += vo
-    
-    fscore, acc = conlleval(output)
-    return fscore, acc
+'''
+This file purpose is to evaluate a heuristic by calculating f1 score and accuracy by using a function named evaluate_heuristic 
+'''
+from library.eval_spans import get_chunk_spans, get_f1_score
 
 def get_tags(file_path):
+    '''
+    This function reads a file with sentences in bracketed notation and gives the BI tags for each sentence in it as a list of lists.
+    Each list in the main list corresponds to each sentence and its BI tags
+    '''
     all_sentence_tags = []
     with(open(file_path, 'r')) as f:
         sentences = f.readlines()
@@ -33,17 +23,36 @@ def get_tags(file_path):
             all_sentence_tags.append(sentence_tags) 
     return all_sentence_tags
 
+
+def evaluate_heuristic(gt_sentences_file, pred_sentences_file):
+    # First we read the input files with sentences in bracketed notations and get the BI tags for them
+    ground_truths = get_tags(gt_sentences_file)
+    predicted_labels = get_tags(pred_sentences_file)
+    # Next we calculate the spans for the ground truths and the predictions
+    ground_truth_spans = [get_chunk_spans(sentence) for sentence in ground_truths]
+    predicted_spans = [get_chunk_spans(sentence) for sentence in predicted_labels]
+    # We use the spans to calculate the f1 score
+    f1  = get_f1_score(ground_truth_spans, predicted_spans)
+    
+    # following is the calculation of accuracy, which is correctly identified tags / total tags 
+    correct_tags = 0
+    total_tags = 0
+
+    for true_tags, pred_tags in zip(ground_truths, predicted_labels):
+        for true_tag, pred_tag in zip(true_tags, pred_tags):
+            total_tags += 1
+            if true_tag == pred_tag:
+                correct_tags += 1
+
+    accuracy = (correct_tags / total_tags) * 100 if total_tags > 0 else 0
+    return f1, accuracy
             
 
 if __name__ == "__main__":
-    # TEST_PATH = "experiments/second_order_hiddendim_anal_FA/test_predicted_0.pkl"
-    TEST_PATH = "data/en_12_tags/test.pkl"
-    TEST_TRUE_TAGS_PATH = "data/en_BI_tags/test.pkl"
+    HEURISTIC_PRED_SENTENCES = 'data\\en_new_notation_pseudo_labels\\test.txt'
+    GROUND_TRUTH_SENTENCES = 'data\\en_new_notation_ground_truths\\test.txt'
 
-    test_data = pickle.load(open(TEST_PATH, 'rb'))
-    test_tags_gt = pickle.load(open(TEST_TRUE_TAGS_PATH, 'rb'))#[:len(test_data)]
-    print(len(test_data), len(test_tags_gt))
-    fscore, acc = eval_hu(test_data, test_tags_gt)
+    fscore, acc = evaluate_heuristic(GROUND_TRUTH_SENTENCES, HEURISTIC_PRED_SENTENCES)
     print( " __________________________________")
     print(f"| Test:")
     print(f"|     F1:       {fscore}")
